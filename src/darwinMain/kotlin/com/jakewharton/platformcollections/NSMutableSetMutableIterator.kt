c64e350cff722317bdbeae557cb4303a4e2ea3b2
Jake Wharton <jw@squareup.com>
2023-03-06 22:42:01 -0500

    Only snapshot set contents if mutating
    Otherwise iterate using the regular enumerator.

--- src/darwinMain/kotlin/com/jakewharton/platformcollections/NSMutableSetMutableIterator.kt
+++ src/darwinMain/kotlin/com/jakewharton/platformcollections/NSMutableSetMutableIterator.kt
@@ -1,28 +1,57 @@
+@file:OptIn(UnsafeNumber::class)
+
 package com.jakewharton.platformcollections
 
+import kotlinx.cinterop.UnsafeNumber
+import platform.Foundation.NSEnumerator
 import platform.Foundation.NSMutableSet
+import platform.Foundation.allObjects
 
 internal class NSMutableSetMutableIterator<E>(
 	private val storage: NSMutableSet,
-	private val iterator: Iterator<Any?>,
 ) : MutableIterator<E> {
+	/* Non-null when iterating without removing. Null after a call to remove(). */
+	private var enumerator: NSEnumerator? = storage.objectEnumerator()
+	/* Empty unless enumerator is null. */
+	private var values: List<E> = emptyList()
+	private var index = 0
+
 	private var canRemove = false
 	private var lastValue: Any? = null
 
-	override fun hasNext() = iterator.hasNext()
+	override fun hasNext() = index < storage.count.toInt()
 
 	override fun next(): E {
-		val next = iterator.next()
-		canRemove = true
-		lastValue = next
-		@Suppress("UNCHECKED_CAST")
-		return next as E
+		if (hasNext()) {
+			val enumerator = enumerator
+			val value = if (enumerator != null) {
+				@Suppress("UNCHECKED_CAST")
+				enumerator.nextObject() as E
+			} else {
+				values[index]
+			}
+			index++
+			canRemove = true
+			lastValue = value
+			return value
+		}
+		throw NoSuchElementException()
 	}
 
 	override fun remove() {
 		check(canRemove)
 		canRemove = false
+
+		if (enumerator != null) {
+			// Darwin APIs do not allow mutation while enumerating. We are forced to snapshot
+			// the values in order to support this case.
+			@Suppress("UNCHECKED_CAST")
+			values = storage.allObjects as List<E>
+			enumerator = null
+		}
+
 		storage.removeObject(lastValue)
+		index--
 		lastValue = null
 	}
 }
