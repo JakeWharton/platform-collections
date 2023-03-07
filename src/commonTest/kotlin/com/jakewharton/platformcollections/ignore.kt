66528380cd109875b5e20fd911460d1c7e82a964
Jake Wharton <jw@squareup.com>
2023-03-06 22:20:33 -0500

    Use new iterator for asMap implementation

--- src/commonTest/kotlin/com/jakewharton/platformcollections/ignore.kt
+++ src/commonTest/kotlin/com/jakewharton/platformcollections/ignore.kt
@@ -3,7 +3,3 @@ package com.jakewharton.platformcollections
 @OptIn(ExperimentalMultiplatform::class)
 @OptionalExpectation
 expect annotation class DarwinIgnore()
-
-@OptIn(ExperimentalMultiplatform::class)
-@OptionalExpectation
-expect annotation class JsIgnore()
